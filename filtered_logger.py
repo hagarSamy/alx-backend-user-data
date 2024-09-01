dbb0a0fcb25c689c295288f6d7a0185371cbc519
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-01 03:11:55 +0300

    Regex-ing

---
+++ filtered_logger.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+''' a function called filter_datum that returns the log message obfuscated'''
+
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    '''returns the log message obfuscated
+    Args:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating
+        all fields in the log line (message)
+    '''
+    for field in fields:
+        message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
+    return message
+            
