f51da64ba1ac2ea0f8522ae2bef6f5ee5c443536
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-01 03:18:54 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,14 +7,7 @@ from typing import List
 
 def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
-    '''returns the log message obfuscated
-    Args:
-        fields: a list of strings representing all fields to obfuscate
-        redaction: a string representing by what the field will be obfuscated
-        message: a string representing the log line
-        separator: a string representing by which character is separating
-        all fields in the log line (message)
-    '''
+    '''returns the log message obfuscated'''
     for field in fields:
         message = re.sub(f"{field}=[^;]*", f"{field}={redaction}", message)
     return message
