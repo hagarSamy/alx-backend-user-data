20479df7d1c603db0817413a208d32eaa894ad40
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-03 20:07:50 +0300

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+from flask import request
+from typing import List, TypeVar
+
+'''auth module'''
+User = TypeVar('User')
+
+
+class Auth:
+    '''a class template for all authentication system'''
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        '''require_auth'''
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        '''authorization_header'''
+        return None
+
+    def current_user(self, request=None) -> User:
+        '''urrent_user'''
+        return None
