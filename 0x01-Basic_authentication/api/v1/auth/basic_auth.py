be6353c684b46081d1471b1c59a931f798eee179
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-04 02:20:38 +0300

    Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 '''BasicAuth module'''
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode
+import re
 
 
 class BasicAuth(Auth):
@@ -17,3 +19,18 @@ class BasicAuth(Auth):
         ):
             return None
         return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                               str) -> str:
+        '''returns the decoded value of
+        a Base64 string base64_authorization_header'''
+        if (
+            base64_authorization_header is None or
+            type(base64_authorization_header) is not str
+        ):
+            return None
+        base64_pattern = re.compile(r'^[A-Za-z0-9+/]*={0,2}$')
+        if not base64_pattern.match(base64_authorization_header):
+            return None
+        return b64decode(base64_authorization_header).decode('utf-8')
