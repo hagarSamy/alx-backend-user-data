ee6c4b806009c9efe2a43817c40ab010b1adb895
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-03 20:52:32 +0300

    require auth func update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,7 +14,11 @@ class Auth:
         if path is not None:
             if path[-1] != '/':
                 path += '/'
-        if path is None or excluded_paths is None or path not in excluded_paths:
+        if (
+            path is None or
+            excluded_paths is None or
+            path not in excluded_paths
+        ):
             return True
         return False
 
