803de06209aa12740af216331481e1043a3812e2
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-07 23:16:55 +0300

    debugging12

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,19 +55,18 @@ def before_request_handler():
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     path = request.path
     if not auth.require_auth(path=path, excluded_paths=excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if (
+        auth.authorization_header(request) is None and
+        auth.session_cookie(request) is None
+    ):
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    # if(
-    #     auth.authorization_header(request) is None and
-    #     auth.session_cookie(request) is None
-    # ):
-    #     abort(401)
     request.current_user = auth.current_user(request)
 
 
