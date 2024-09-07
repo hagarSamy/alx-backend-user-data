80a12f38979b6d379347eaa5ef2154709809b99e
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-07 14:36:22 +0300

    Et moi et moi et moi!

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -29,7 +29,9 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     if user_id == 'me' and request.current_user is None:
         abort(404)
-    if user_id != 'me' or (user_id == 'me' and request.current_user is not None):
+    if user_id != 'me' or (
+                            user_id == 'me' and
+                            request.current_user is not None):
         user = User.get(user_id)
         if user is None:
             abort(404)
