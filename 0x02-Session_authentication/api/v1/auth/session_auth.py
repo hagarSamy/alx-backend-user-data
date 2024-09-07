fad7f8391cd309426f57ac232bd0a38fae6fd40d
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-08 00:07:20 +0300

    identifying a user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -34,7 +34,7 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None):
         '''returns a User instance based on a cookie value'''
-        session_id  = self.session_cookie(request)
+        session_id = self.session_cookie(request)
         user_id = self.user_id_for_session_id(session_id)
         user = User.get(user_id)
         return user
