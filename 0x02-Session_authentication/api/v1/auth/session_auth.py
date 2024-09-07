74947a9164f488e2e3c1ed4179da4ce55e9d9354
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-07 15:23:29 +0300

    sessionAuth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,22 @@
 '''session_auth module'''
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     '''session auth class that will create a new auth mechanism to
     - validate if everything inherits correctly without any overloading
     - validate the “switch” by using environment variables'''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''creates a Session ID for a user_id'''
+        if(
+            user_id is None or
+            type(user_id) is not str
+        ):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
