bd22c60151300dfe583e1484cca834865138eab1
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-11 02:06:42 +0300

    find user by kwargs

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -36,3 +37,15 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs):
+        '''takes in arbitrary keyword arguments and
+        returns the first row found in the users table as
+        filtered by the method’s input arguments'''
+        try:
+            row = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound
+        except InvalidRequestError:
+            raise InvalidRequestError
+        return row
