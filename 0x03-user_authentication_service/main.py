e7fc935ad6930537291578a36e047b85995629ec
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-11 00:58:31 +0300

    main1

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -2,20 +2,14 @@
 """
 Main file
 """
-# from user import User
+from user import User
 
 # print(User.__tablename__)
 
 # for column in User.__table__.columns:
 #     print("{}: {}".format(column, column.type))
 
-#!/usr/bin/env python3
-"""
-Main file
-"""
-
 from db import DB
-from user import User
 
 my_db = DB()
 
