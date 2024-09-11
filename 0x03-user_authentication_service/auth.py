f3c8f9fd44b5897a1398ba98ea88e85f47e2967e
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-12 01:42:58 +0300

    uuid gen

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,6 +19,11 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+        '''return a string representation of a new UUID'''
+        return str(uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -47,7 +52,3 @@ class Auth:
             return bcrypt.checkpw(bytes_password, user.hashed_password)
         except Exception:
             return False
-
-    def _generate_uuid() -> str:
-        '''return a string representation of a new UUID'''
-        return str(uuid4())
