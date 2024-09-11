610751743751427d392e1251c36ba444bb955b9c
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-12 01:42:06 +0300

    uuid gen

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -48,6 +48,6 @@ class Auth:
         except Exception:
             return False
 
-    def _generate_uuid():
+    def _generate_uuid() -> str:
         '''return a string representation of a new UUID'''
         return str(uuid4())
