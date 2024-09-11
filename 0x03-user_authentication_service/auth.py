13b821fe930d88f4aee80c1ebc42ac1bc38f060e
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-11 03:29:10 +0300

    hashing pswd

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+hashing password
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    '''hashing pswd'''
+    # Convert the password string to bytes
+    password_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+    return hashed_password
