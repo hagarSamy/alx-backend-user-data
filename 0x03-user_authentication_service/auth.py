7d74ea15b565ee8fb71eef7d9cbf81b39b096c11
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-11 05:04:14 +0300

    main5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """
-hashing password
+auth
 """
 
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,3 +15,15 @@ def _hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        '''init'''
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        '''registering a user'''
+        
