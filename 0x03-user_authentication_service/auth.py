88e6fb8887150ddadec328b637edbc9f5398eb11
hagarSamy <hs1800@fayoum.edu.eg>
2024-09-12 12:43:50 +0300

    filter by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,9 +68,7 @@ class Auth:
         if session_id is None:
             return None
         try:
-            user = self._db.find_user_by(session_id)
-            if user is None:
-                return None
+            user = self._db.find_user_by(session_id=session_id)
             return user
         except NoResultFound:
             return None
